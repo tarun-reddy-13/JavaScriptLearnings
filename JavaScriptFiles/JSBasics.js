0db6474b1864840ec7637bbdb8e970ff762f7983
Tarun <tarunreddythota13@gmail.com>
2025-03-13 02:55:44 +0530

    updated till variable naming convention

---
+++ JavaScriptFiles/JSBasics.js
@@ -0,0 +1,53 @@
+/*
+"Hello", 'Hello' both are accepted
+*/
+
+console.log("Hello World");
+console.log("Started to learn JavaScrpt");
+
+/*
+
+Variables in JS
+-> VAriables are containers(memory location) for data
+-> JS is a dynamically typed language
+
+*/
+
+//Variable declaration
+name = "Tarun";
+console.log(name);
+
+age = 10;
+console.log(age);
+
+price = 99.9
+console.log(price);
+
+x=null;
+console.log(x);
+
+y=undefined;
+console.log(y);
+
+//boolean values --> true/false
+isTrue = true;
+console.log(isTrue);
+
+isTrue = false;
+console.log(isTrue);
+
+/*
+
+ '=' is an assignment operator
+ variable = value
+ (memory location) = (value to be stored)
+
+ Variable naming convention:
+ 
+ * Variable names are case sensitive: "a" is different from "A"
+ * Only letters, digits, underscore(_) and $ are allowed.
+ * Onlu a letter, underscore or $ should be 1st character of variable.
+ eg: name, _val, $value, name1 (valid names for variable)
+     12value, my name (invalid)
+ * Reserved words cannot be variable names.
+*/
