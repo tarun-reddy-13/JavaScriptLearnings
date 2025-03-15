9f1a97b3eb11f6b2e486eb06f0956790ff4a6d26
Tarun <tarunreddythota13@gmail.com>
2025-03-15 20:52:16 +0530

    updated about DOM Manipulation

--- DOM_Concepts/Project1/script.js
+++ DOM_Concepts/Project1/script.js
@@ -10,5 +10,21 @@ It is a global object with lots of properties and methods.
 
 */
 
+/*
 
-alert("hello!!");
+manipulating using dom
+
+*/
+
+let headding = document.querySelector("h2");
+headding_text = headding.innerText;
+let new_headding = headding_text+", from learners";
+headding.innerText = new_headding;
+
+let containers = document.querySelectorAll(".box");
+
+let idx=1;
+for(let container of containers){
+    container.innerHTML += `<p><i>unique content in div ${idx}<i><p>`; 
+    idx++;
+}
