f6d0bb14f06487585111c9e803a58ea63881101e
Tarun <tarunreddythota13@gmail.com>
2025-03-15 15:28:42 +0530

    started DOM

---
+++ DOM_Concepts/Project1/script.js
@@ -0,0 +1,14 @@
+/*
+
+Window Object:
+--------------
+
+The window object represents an open window in a browser. It is browser's object(not JS)
+& is automatically created by browser.
+
+It is a global object with lots of properties and methods.
+
+*/
+
+
+alert("hello!!");
